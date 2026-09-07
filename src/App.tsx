3d7f199e73b808eca95a399f233f169e2075a563
Natka2091 <fomnata41@.com>
2026-09-07 21:26:14 +0100

    downloaded icons and pictures

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,7 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-900 text-white p-4">
-      <div className="text-center space-y-4">
-        <h1 className="text-4xl md:text-6xl font-black text-cyan-400 animate-bounce">
-          🎮 TS ПРОЄКТ ГОТОВИЙ!
-        </h1>
-        <p className="text-xl text-slate-300 max-w-md mx-auto">
-          React + Vite + TypeScript + Tailwind v4 активовані.
-        </p>
-      </div>
-    </div>
+    <></>
   );
 }
 
